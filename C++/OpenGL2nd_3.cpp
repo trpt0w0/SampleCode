b7462f3c2eb100ef3dd269bdd4595363499d900c
tprt0w0 <x7dsx@icloud.com>
2020-01-04 20:35:17 +0900

    openGL2nd_3 追加

--- C++/OpenGL2nd_3.cpp
+++ C++/OpenGL2nd_3.cpp
@@ -16,19 +16,25 @@ void main(){
 		scanf("%d", &player_hand);
 
 		// 乱数を使ってCPUの手を選択
-		int cpu_hand = std::uniform_int_distribution<>(0, 2);
+		int cpu_hand = std::uniform_int_distribution<>(0, 2)(rand);
 		
 		// すべての勝敗の結果を表示し、勝敗回数を記録
-		if(player_hand == 2 && cpu_hand == 0){
-			printf("あなたの勝ち\n");
-			++win;
+		if(player_hand == cpu_hand)
+		{	
+			printf("引き分けです\n");
+		}else if(player_hand == 2 && cpu_hand == 0  || player_hand == 1 && cpu_hand == 2 || player_hand == 2 && cpu_hand == 1){
+				printf("あなたの勝ち\n");
+				++win;
+		}else{
+			printf("あなたの負け\n");
+				lose++;
 		}
+
 
 	}
 	printf("勝ち=%d 負け=%d 引き分け=%d \n", win, lose, max_play_count - win- lose);
 	if(win > lose){
 		printf("あなたの勝ちです.\n");
-
 		// 負ける場合の条件
 	}else if(lose > win){
 		printf("あなたの負けです.\n");
